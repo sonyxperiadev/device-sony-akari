c5917d5a992c833cc4a6aafd1887bf180731379b
Pavel Dubrova <pashadubrova@gmail.com>
2021-12-15 06:01:31 +0200

    device: Set TARGET_VIBRATOR_V1_2 flag
    Starting with 4.19 kernel Akari device uses QTI Haptics driver
    and should use QTI vibrator HAL accordingly.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ DEVICE_PATH := device/sony/akari/rootdir
 DEVICE_PACKAGE_OVERLAYS += \
     device/sony/akari/overlay
 
+# Vibrator
+TARGET_VIBRATOR_V1_2 := true
+
 # Device Specific Permissions
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
